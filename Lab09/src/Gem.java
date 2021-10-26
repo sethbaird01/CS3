f07f748b5776e0a272ecff67ada55850fddafa84
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-10-26 16:16:52 -0500

    Lab 09 Complete

--- Lab09/src/Gem.java
+++ Lab09/src/Gem.java
@@ -1,32 +1,72 @@
-import java.awt.Font;
+
+// import java.awt.Font;
+import java.awt.Color;
+import java.util.Random;
 
 enum GemType {
-    GREEN, BLUE, ORANGE; //define the different types of Gems, comma delimited
+	GREEN, BLUE, ORANGE; // define the different types of Gems, comma delimited
 }
 
-public class Gem 
-{	
+public class Gem { //tested working
+
+	private int value;
+	private GemType type;
+
+	static final int[] possibleValues = { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+	static final Random rand = new Random();
+	static final String imageParentFolder = "/Users/seth/Documents/GitHub/CS3/Lab09/src/";
+
+	public Gem() {
+		this.value = possibleValues[rand.nextInt(possibleValues.length)];
+		this.type = GemType.values()[rand.nextInt(3)];
+	}
+
+	public Gem(GemType type, int points) {
+		this.value = points;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return type + " " + value;
+	}
+
+	public GemType getType() {
+		return this.type;
+	}
+
+	public int getPoints() {
+		return this.value;
+	}
+
+	public void draw(double x, double y) {
+		String path = imageParentFolder + "gem_" + this.type.toString().toLowerCase() + ".png";
+		StdDraw.picture(x, y, path);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.text(x, y, String.valueOf(this.value));
+		StdDraw.setPenColor(Color.BLACK);
+
+	}
+
 	/** Tester main method */
-	public static void main(String [] args)
-	{
-//		final int maxGems = 16;
-//		
-//		// Create a gem of each type
-//		Gem green  = new Gem(GemType.GREEN, 10);
-//		Gem blue   = new Gem(GemType.BLUE, 20);
-//		Gem orange = new Gem(GemType.ORANGE, 30);
-//		System.out.println(green  + ", " + green.getType()  + ", " + green.getPoints());		
-//		System.out.println(blue   + ", " + blue.getType()   + ", " + blue.getPoints());
-//		System.out.println(orange + ", " + orange.getType() + ", " + orange.getPoints());
-//		green.draw(0.3, 0.7);
-//		blue.draw(0.5, 0.7);
-//		orange.draw(0.7, 0.7);
-//		
-//		// A row of random gems
-//		for (int i = 0; i < maxGems; i++)
-//		{
-//			Gem g = new Gem();
-//			g.draw(1.0 / maxGems * (i + 0.5), 0.5);
-//		}
+	public static void main(String[] args) {
+		final int maxGems = 16;
+
+		// Create a gem of each type
+		Gem green = new Gem(GemType.GREEN, 10);
+		Gem blue = new Gem(GemType.BLUE, 20);
+		Gem orange = new Gem(GemType.ORANGE, 30);
+		System.out.println(green + ", " + green.getType() + ", " + green.getPoints());
+		System.out.println(blue + ", " + blue.getType() + ", " + blue.getPoints());
+		System.out.println(orange + ", " + orange.getType() + ", " + orange.getPoints());
+		green.draw(0.3, 0.7);
+		blue.draw(0.5, 0.7);
+		orange.draw(0.7, 0.7);
+
+		// A row of random gems
+		for (int i = 0; i < maxGems; i++) {
+			Gem g = new Gem();
+			g.draw(1.0 / maxGems * (i + 0.5), 0.5);
+		}
 	}
 }
--- Lab09/src/GemList.java
+++ Lab09/src/GemList.java
@@ -1,40 +1,205 @@
-public class GemList 
-{
-	public static void main(String [] args)
-	{
-//		GemList list = new GemList();
-//		System.out.println(list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.9);		
-//		
-//		list.insertBefore(new Gem(GemType.BLUE, 10), 0);
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.8);
-//		
-//		list.insertBefore(new Gem(GemType.BLUE, 20), 99);  //not a mistake, should still work
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.7);
-//		
-//		list.insertBefore(new Gem(GemType.ORANGE, 30), 1);
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.6);
-//		
-//		list.insertBefore(new Gem(GemType.ORANGE, 10), 2);
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.5);
-//		
-//		list.insertBefore(new Gem(GemType.ORANGE, 50), 3);
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.4);
-//		
-//		list.insertBefore(new Gem(GemType.GREEN, 50), 2);
-//		System.out.println("\n" + list);
-//		System.out.println("size = " + list.size() + ", score = " + list.score());
-//		list.draw(0.3);		
-	}	
+public class GemList {
+
+	private class Node {
+		private Gem gem;
+		private Node next;
+
+		Node(Gem in) {
+			this.gem = in;
+		}
+
+		@Override
+		public String toString() {
+			return this.gem.toString();
+		}
+	}
+
+	private Node head;
+	private int size;
+
+	public GemList() {
+		this.size = 0;
+		this.head = null;
+	}
+
+	public GemList(Gem i) {
+		this.size = 1;
+		this.head = new Node(i);
+	}
+
+	public int getSize() {
+		return this.size;
+	}
+
+	public void insertBefore(Gem i, int idx) {// tested working
+		int index = 0;
+		this.size++;
+
+		if (this.head == null) {
+			// goal index irrelevant, just insert element
+			this.head = new Node(i);
+
+			return;
+		}
+
+		if (idx <= 0) { //edge case, clicking before the first element
+			Node temp = this.head;
+			this.head = new Node(i);
+			this.head.next = temp;
+
+			return;
+		}
+
+		// add to last element
+		if (idx >= size) {
+			Node current = this.head;
+			while (current.next != null) {
+				current = current.next;
+			} // [1, 2, 3, null]
+			current.next = new Node(i);
+
+			return;
+		}
+
+		// add to target index
+		Node current = this.head;
+		while (current.next != null) {
+			if (index == idx - 1) {
+				Node temp = current.next;
+				current.next = new Node(i);
+				current.next.next = temp;
+
+				return;
+			}
+			current = current.next;
+			index++;
+		}
+		if (index == idx - 1) {
+			Node temp = current.next;
+			current.next = new Node(i);
+			current.next.next = temp;
+
+			return;
+		}
+
+		System.out.println("error adding element, index: " + idx);
+	}
+
+	public int score() {// tested working
+
+		if (this.head == null) {
+			return 0;
+		}
+
+		int out = 0;
+		Node current = this.head;
+		int currMulti = 0;
+		int multiLen = 0;
+		while (current.next != null) {
+			if (current.gem.getType().equals(current.next.gem.getType())) {
+				// multiplier goes here
+				if (currMulti != 0) {// continuing multiplier, increment len and add to currMulti
+					currMulti += current.gem.getPoints();
+					multiLen++;
+				} else {// beginning multiplier, change values accordingly
+					currMulti = current.gem.getPoints();
+					multiLen = 1;
+				}
+			} else {
+
+				if (currMulti != 0) {// multiplier over, take currMulti * multiLen and add it to `out`
+					currMulti += current.gem.getPoints();
+					multiLen++;
+					out += (currMulti * multiLen);
+					currMulti = 0;
+					multiLen = 0;
+				} else {// there is no multiplier, just add value
+					out += current.gem.getPoints();
+				}
+			}
+			current = current.next;
+		}
+
+		if (currMulti != 0) {// multiplier over, take currMulti * multiLen and add it to `out`
+			currMulti += current.gem.getPoints();
+			multiLen++;
+			out += (currMulti * multiLen);
+			currMulti = 0;
+			multiLen = 0;
+		} else {// there is no multiplier, just add value
+			out += current.gem.getPoints();
+		}
+
+		// my brain hurts why is this so complicated
+		return out;
+	}
+
+	public void draw(double y) { // tested working
+		final int maxGems = 16;
+		Node current = this.head;
+		int x = 0;
+		if (head == null) {
+			return;
+		}
+		while (current.next != null) {
+			Gem g = current.gem;
+			g.draw(1.0 / maxGems * (x++ + 0.5), y);
+			current = current.next;
+		}
+		Gem g = current.gem;
+		g.draw(1.0 / maxGems * (x++ + 0.5), y);
+	}
+
+	@Override
+	public String toString() { // tested working
+		Node current = this.head;
+		String out = "";
+		if (current == null) {
+			return "";
+		}
+		while (current.next != null) {
+			out += current.gem.getType() + " -> ";
+			current = current.next;
+		}
+		out += current.gem.getType() + " -> ";
+
+		return out.substring(0, out.length() - 4);
+	}
+
+	public static void main(String[] args) {
+		GemList list = new GemList();
+		System.out.println(list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.9);
+
+		list.insertBefore(new Gem(GemType.BLUE, 10), 0);
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.8);
+
+		list.insertBefore(new Gem(GemType.BLUE, 20), 99); // not a mistake, should still work
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.7);
+
+		list.insertBefore(new Gem(GemType.ORANGE, 30), 1);
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.6);
+
+		list.insertBefore(new Gem(GemType.ORANGE, 10), 2);
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.5);
+
+		list.insertBefore(new Gem(GemType.ORANGE, 50), 3);
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.4);
+
+		list.insertBefore(new Gem(GemType.GREEN, 50), 2);
+		System.out.println("\n" + list);
+		System.out.println("size = " + list.getSize() + ", score = " + list.score());
+		list.draw(0.3);
+	}
 }

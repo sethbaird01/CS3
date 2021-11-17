5d65870ab2df35e636d5de1848b8dcd60ff9f8d7
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-11-17 15:25:12 -0600

    Lab 12 Initial commit

---
+++ Lab12/src/BoggleSolver.java
@@ -0,0 +1,46 @@
+public class BoggleSolver
+{
+	// Initializes the data structure using the given array of strings as the dictionary.
+	// (You can assume each word in the dictionary contains only the uppercase letters A - Z.)
+	public BoggleSolver(String dictionaryName)
+	{
+		//TODO
+	}
+
+	// Returns the set of all valid words in the given Boggle board, as an Iterable object
+	public Iterable<String> getAllValidWords(BoggleBoard board)
+	{
+		//TODO
+
+		return null;
+	}
+
+	// Returns the score of the given word if it is in the dictionary, zero otherwise.
+	// (You can assume the word contains only the uppercase letters A - Z.)
+	public int scoreOf(String word)
+	{
+		//TODO
+
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("WORKING");
+
+		final String PATH   = "./data/";
+		BoggleBoard  board  = new BoggleBoard(PATH + "board-q.txt");
+		BoggleSolver solver = new BoggleSolver(PATH + "dictionary-algs4.txt");
+
+		int totalPoints = 0;
+
+		for (String s : solver.getAllValidWords(board)) {
+			System.out.println(s + ", points = " + solver.scoreOf(s));
+			totalPoints += solver.scoreOf(s);
+		}
+
+		System.out.println("Score = " + totalPoints); //should print 84
+
+		//new BoggleGame(4, 4);
+	}
+
+}

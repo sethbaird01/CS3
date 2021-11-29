b1afa72d6d53bf61da0bf626895a18d6ac9cc8a9
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-11-28 18:15:52 -0600

    incremental commit

--- Lab12/src/BoggleSolver.java
+++ Lab12/src/BoggleSolver.java
@@ -1,90 +1,90 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 
-public class BoggleSolver
-{
+public class BoggleSolver {
 	Dictionary d;
+	// BoggleBoard b;
+	HashSet<String> set;
 
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A - Z.)
-	public BoggleSolver(String dictionaryName)
-	{
+	// Initializes the data structure using the given array of strings as the
+	// dictionary.
+	// (You can assume each word in the dictionary contains only the uppercase
+	// letters A - Z.)
+	public BoggleSolver(String dictionaryName) {
 		try {
-			d  = new Dictionary(new File(dictionaryName));
+			d = new Dictionary(new File(dictionaryName));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary file not found");
 		}
 	}
 
-	// Returns the set of all valid words in the given Boggle board, as an Iterable object
-	public Iterable<String> getAllValidWords(BoggleBoard board)
-	{
-		//hashset stores all words
-		HashSet<String> set = new HashSet<String>();
+	// Returns the set of all valid words in the given Boggle board, as an Iterable
+	// object
+	public Iterable<String> getAllValidWords(BoggleBoard board) {
+		// hashset stores all words
+		set = new HashSet<String>();
 
-		for (int r = 0; r < board.rows(); r++) {
-			for (int c = 0; c < board.cols(); c++) {
-				recurse(board, r, c);
-			}
-		}
+		findWords(board, d);
 
-		//returning hashset which now has all combinations
+		// returning hashset which now has all combinations
 		return set;
 	}
 
-	List<Character> getNeighbors(BoggleBoard board, int r, int c){
-        List<Character> list = new ArrayList<Character>();
-    try {
-        list.add(board.getLetter(r-1, c));
-    } catch (Exception e) {}
-    try {
-        list.add(board.getLetter(r, c-1));
-    } catch (Exception e) {}
-        //current
-    try {
-        list.add(board.getLetter(r, c+1));
-    } catch (Exception e) {}
-    try {
-        list.add(board.getLetter(r+1, c));
-    } catch (Exception e) {}
+	// recursive word finding algorithm
+	void findWordsUtil(BoggleBoard b, boolean visited[][], int i, int j, String str, Dictionary d) {
+		int rows = b.rows(), cols = b.cols();
 
-    return list;
-    }
+		// mark current cell as visited and add current character to str
+		visited[i][j] = true;
+		str += b.getLetter(i, j);
 
-	public List<String> recurse(BoggleBoard b, int x, int y, List<String> list){
-
-
-		//replace list, implement using the hashset fmor above
-		String currWord = "";
-
-		//returns all valid words starting with selected lexstter
-		//maybe checks against hashset then no need to return array
-		//new hashset per board
-
-		if(currWord.length() > 2 && d.doesExist(currWord) ){
-			list.add(currword);
+		// add current string if it meets criteria
+		if (str.length() > 2 && d.doesExist(str)) {
+			this.set.add(str);
 		}
 
+		// check 8 neighboring squares of boggle[i][j]
+		for (int row = (i - 1); row <= (i + 1) && row < rows; row++) {
+			for (int col = (j - 1); col <= (j + 1) && col < cols; col++) {
+				if (row >= 0 && col >= 0 && !visited[row][col]) {
+					findWordsUtil(b, visited, row, col, str, d);
+				}
+			}
+		}
 
-		return recurse(b, /* TODO */, list);
+		// remove current character from string and mark current cell's visited to false
+		str = "" + str.charAt(str.length() - 1);
+		visited[i][j] = false;
 	}
 
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A - Z.)
-	public int scoreOf(String word)
-	{
-		if(!d.doesExist(word)){
+	// finds all words present in dictionary (helper method)
+	void findWords(BoggleBoard b, Dictionary d) {
+
+		int rows = b.rows(), cols = b.cols();
+		// mark all characters not visited
+		boolean visited[][] = new boolean[b.rows()][b.cols()];
+		String str = "";
+
+		// go over every character in board,
+		// then look for all words starting with this character
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				findWordsUtil(b, visited, i, j, str, d);
+			}
+		}
+	}
+
+	// returns the score of the given word if its in the dictionary, zero otherwise.
+	public int scoreOf(String word) {
+		if (!d.doesExist(word)) {
 			return 0;
 		}
 
 		switch (word.length()) {
 			case 0:
 				return 0;
-		
+
 			case 1:
 				return 0;
 
@@ -105,7 +105,7 @@ public class BoggleSolver
 
 			case 7:
 				return 5;
-		
+
 			default: // 8 or greater
 				return 11;
 		}
@@ -115,8 +115,8 @@ public class BoggleSolver
 	public static void main(String[] args) {
 		System.out.println("WORKING");
 
-		final String PATH   = "./data/";
-		BoggleBoard  board  = new BoggleBoard(PATH + "board-q.txt");
+		final String PATH = "./data/";
+		BoggleBoard board = new BoggleBoard(PATH + "board-q.txt");
 		BoggleSolver solver = new BoggleSolver(PATH + "dictionary-algs4.txt");
 
 		int totalPoints = 0;
@@ -126,9 +126,9 @@ public class BoggleSolver
 			totalPoints += solver.scoreOf(s);
 		}
 
-		System.out.println("Score = " + totalPoints); //should print 84
+		System.out.println("Score = " + totalPoints); // should print 84
 
-		//new BoggleGame(4, 4);
+		// new BoggleGame(4, 4);
 	}
 
 }

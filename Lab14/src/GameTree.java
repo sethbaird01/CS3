2925d75bfe47067f956ac03e3d29ca0ac07cb5a4
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2022-01-21 16:05:36 -0600

    Lab 14 Complete

--- Lab14/src/GameTree.java
+++ Lab14/src/GameTree.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
@@ -7,18 +9,54 @@ import java.util.Scanner;
  *
  * @author Rick Mercer
  */
-public class GameTree
-{
+public class GameTree {
+
+	Node root, current;
+	String fileName;
+
+	private class Node {
+		String data;
+		Node left, right;
+
+		public Node(String data) {
+			this.data = data;
+			left = right = null;
+		}
+	}
+
 	/**
 	 * Constructor needed to create the game.
 	 *
 	 * @param fileName
-	 *          this is the name of the file we need to import the game questions
-	 *          and answers from.
+	 *                 this is the name of the file we need to import the game
+	 *                 questions
+	 *                 and answers from.
 	 */
-	public GameTree(String fileName)
-	{
-		//TODO
+	public GameTree(String fileName) {
+		try {
+			this.fileName = fileName;
+			File obj = new File(fileName);
+			Scanner reader = new Scanner(obj);
+			root = readIn(reader);
+			reader.close();
+			current = root;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Node readIn(Scanner reader) {
+		if (!reader.hasNextLine()) {
+			return null;
+		}
+
+		String currLine = reader.nextLine().trim();
+		Node curr = new Node(currLine);
+		if (curr.data.endsWith("?")) {
+			curr.right = readIn(reader);
+			curr.left = readIn(reader);
+		}
+		return curr;
 	}
 
 	/*
@@ -28,18 +66,20 @@ public class GameTree
 	 */
 	// -----------Feathers?-----------------Feathers?------
 	// -------------/----\------------------/-------\------
-	// ------- chicken  horse-----Does it swim?-----horse--
+	// ------- chicken horse-----Does it swim?-----horse--
 	// -----------------------------/------\---------------
 	// --------------------------goose--chicken-----------
 	/**
 	 * @param newQ
-	 *          The question to add where the old answer was.
+	 *             The question to add where the old answer was.
 	 * @param newA
-	 *          The new Yes answer for the new question.
+	 *             The new Yes answer for the new question.
 	 */
-	public void add(String newQ, String newA)
-	{
-		//TODO
+	public void add(String newQ, String newA) {
+		String temp = current.data;
+		current.data = newQ;
+		current.right = new Node(newA);
+		current.left = new Node(temp);
 	}
 
 	/**
@@ -48,24 +88,20 @@ public class GameTree
 	 * @return False if the current node is an internal node rather than an answer
 	 *         at a leaf.
 	 */
-	public boolean foundAnswer()
-	{
-		//TODO
+	public boolean foundAnswer() {
+		return !current.data.endsWith("?");
 
-		return false; //replace
 	}
 
 	/**
 	 * Return the data for the current node, which could be a question or an
-	 * answer.  Current will change based on the users progress through the game.
+	 * answer. Current will change based on the users progress through the game.
 	 *
 	 * @return The current question or answer.
 	 */
-	public String getCurrent()
-	{
-		//TODO
+	public String getCurrent() {
+		return current.data;
 
-		return ""; //replace
 	}
 
 	/**
@@ -74,26 +110,37 @@ public class GameTree
 	 *
 	 * @param yesOrNo
 	 */
-	public void playerSelected(Choice yesOrNo)
-	{
-		//TODO
+	public void playerSelected(Choice yesOrNo) {
+		current = yesOrNo == Choice.Yes ? current.right : current.left;
 	}
 
 	/**
 	 * Begin a game at the root of the tree. getCurrent should return the question
 	 * at the root of this GameTree.
 	 */
-	public void reStart()
-	{
-		//TODO
+	public void reStart() {
+		current = root;
 	}
 
 	@Override
-	public String toString()
-	{
-		//TODO
+	public String toString() {
+		// used stringbuilder this time
+		StringBuilder sb = new StringBuilder("");
+		stringHelper(sb, root, 0);
+		return sb.toString();
+	}
 
-		return "";
+	public void stringHelper(StringBuilder sb, Node curr, int depth) {
+		if (curr != null) {
+			stringHelper(sb, curr.left, depth + 1);
+			StringBuilder indent = new StringBuilder("");
+			for (int i = 0; i < depth; i++) {
+				indent.append("- ");
+			}
+			sb.append(indent);
+			sb.append(curr.data + "\n");
+			stringHelper(sb, curr.right, depth + 1);
+		}
 	}
 
 	/**
@@ -101,8 +148,22 @@ public class GameTree
 	 * have new questions added since the game started.
 	 *
 	 */
-	public void saveGame()
-	{
-		//TODO
+	public void saveGame() {
+		PrintWriter out = null;
+		try {
+			out = new PrintWriter(new File(fileName));
+			saveHelper(out, root);
+			out.close();
+		} catch (IOException io) {
+			System.out.println("IOException: " + fileName);
+		}
+	}
+
+	private void saveHelper(PrintWriter out, Node root) {
+		if (root != null) {
+			out.println(root.data);
+			saveHelper(out, root.left);
+			saveHelper(out, root.right);
+		}
 	}
 }
